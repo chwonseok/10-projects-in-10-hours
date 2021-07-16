94d25ba8533507870e0c7ac4b9e6f5d2900760d7
cws0316 <dev.chwonseok@gmail.com>
2021-07-16 10:13:52 +0900

    Started todo app

---
+++ todo-app/script.js
@@ -0,0 +1,17 @@
+const title = document.getElementById('title'),
+  form = document.getElementById('form'),
+  input = document.getElementById('input'),
+  todos = document.getElementById('todos');
+
+const formHandler = (e) => {
+  e.preventDefault();
+  if (!input.value) return;
+
+  const todoEl = document.createElement('li');
+
+  todos.appendChild(todoEl);
+  todoEl.innerText = input.value;
+  input.value = '';
+};
+
+form.addEventListener('submit', formHandler);
